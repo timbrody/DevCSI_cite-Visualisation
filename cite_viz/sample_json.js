10e8cc05241cde45e2dc325c2bad3f5b2e555fce
Edward Minnett <edward@ejmdesign.co.uk>
2012-09-28 14:45:14 +0100

    Added visualisation wrapper.

---
+++ cite_viz/sample_json.js
@@ -0,0 +1,53 @@
+var citesThis = {id: "node02",
+        name: "0.2",
+        data: {"size": 5},
+        children: [{
+			"id": 1,
+			"name": "Иосиф",
+			"data": {
+			"distance": 3,
+			"size": 81
+			},
+			"children": []
+			},
+			{
+			"id": 2,
+			"name": "Иннокентий",
+			"data": {
+			"distance": 9,
+			"size": 7
+			},
+			"children": [ ]
+			},
+			{
+			"id": 3,
+			"name": "Анатолий",
+			"data": {
+			"distance": 6,
+			"size": 85
+			},
+			"children": [ ]
+			},
+			{
+			"id": 4,
+			"name": "Павел",
+			"data": {
+			"distance": 9,
+			"size": 29
+			},
+			"children": [ ]
+			},
+			{
+			"id": 5,
+			"name": "Эдвард",
+			"data": {
+			"distance": 8,
+			"size": 58
+			},
+			"children": [ ]
+			}
+	]
+};
+
+var thisCites = {
+};
